993d5e58e3ab108966d53d2792b7bd94e6eb4f4a
Adam Shannon <adam@ashannon.us>
2011-04-10 09:47:57 -0500

    Update with what I think is a correct solution.

--- script.py
+++ script.py
@@ -21,10 +21,40 @@ def shuffleDoors(lyst):
 			lyst[x] = 'n'
 		
 gameCount = validateInt("Enter the number of games: ", "Enter a number")
+gamesWon = 0
 gamesWonWithSwitch = 0
+gamesLostWithSwitch = 0
 gamesWonWithoutSwitch = 0
+gamesLostWithoutSwitch = 0
 doors = ['n','n','n']
+doorToChoose = randint(0,2)
+swap = randint(0,1)
 
 for x in xrange(gameCount):
 	shuffleDoors(doors)
-	# ...	
+	# Pick one door
+	if doors[doorToChoose] == 'y':
+		gamesWon += 1
+	else:
+		leftover = [0,1,2]
+		leftover.remove(doorToChoose)
+		newDoorToChoose = leftover[randint(0,1)]
+		if (doorToChoose != newDoorToChoose) and doors[newDoorToChoose] == 'y':
+			gamesWonWithSwitch += 1
+		if (doorToChoose != newDoorToChoose) and doors[newDoorToChoose] == 'n':
+			gamesLostWithSwitch += 1
+		if (doorToChoose == newDoorToChoose) and doors[newDoorToChoose] == 'y':
+			gamesWonWithoutSwitch += 1
+		if (doorToChoose == newDoorToChoose) and doors[newDoorToChoose] == 'n':
+			gamesLostWithoutSwitch += 1
+
+print "\n\n"
+print "Games won flatout: %d" % gamesWon
+print "Games Won With Switch: %d" % gamesWonWithSwitch
+print "Games Lost With Switch: %d" % gamesLostWithSwitch
+
+print "\n\n"
+print "=" * 3 + "Totals" + "=" * 3
+print "Games Won: %d" % (gamesWon + gamesWonWithSwitch)
+print "Games Lost: %d" % gamesLostWithSwitch
+print "\n\n"
